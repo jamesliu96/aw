330ae28e024eb00330fd5ee69f3f4445ceaf7453
James Liu <jamesliu96@users.noreply.github.com>
2022-01-11 18:44:26 +0800

    feat: supports emoji

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,13 @@ enum Op {
   Decode,
 }
 
-function toBin(s: string) {
-  const codes = new Uint16Array(s.length);
-  for (let i = 0; i < codes.length; i++) codes[i] = s.charCodeAt(i);
+function toBin(str: string) {
+  const codes = new Uint16Array(str.length);
+  for (let i = 0; i < codes.length; i++) codes[i] = str.charCodeAt(i);
   return btoa(String.fromCharCode(...new Uint8Array(codes.buffer)));
 }
-function fromBin(s: string) {
-  const binary = atob(s);
+function fromBin(str: string) {
+  const binary = atob(str);
   const bytes = new Uint8Array(binary.length);
   for (let i = 0; i < bytes.length; i++) bytes[i] = binary.charCodeAt(i);
   return String.fromCharCode(...new Uint16Array(bytes.buffer));
@@ -23,18 +23,18 @@ function fromBin(s: string) {
 
 const b65 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
 
-const encode = (s: string, dict65: string): string =>
-  s
-    .split('')
-    .map((s) => dict65.charAt(b65.indexOf(s)))
+const s2 = (s: string) => [...s];
+
+const encode = (str: string, dict65: string): string =>
+  s2(str)
+    .map((s) => s2(dict65)[b65.indexOf(s)])
     .join('');
-const decode = (s: string, dict65: string): string =>
-  s
-    .split('')
-    .map((s) => b65.charAt(dict65.indexOf(s)))
+const decode = (str: string, dict65: string): string =>
+  s2(str)
+    .map((s) => b65.charAt(s2(dict65).indexOf(s)))
     .join('');
-const validate = (s: string, dict65: string): boolean =>
-  !s.split('').some((s) => dict65.indexOf(s) < 0);
+const validate = (str: string, dict65: string): boolean =>
+  !s2(str).some((s) => s2(dict65).indexOf(s) < 0);
 
 function run(op: Op, dict65: string, input: string): string {
   switch (op) {
@@ -42,19 +42,22 @@ function run(op: Op, dict65: string, input: string): string {
       return encode(toBin(input), dict65);
     }
     case Op.Decode: {
-      if (!validate(input, dict65)) throw new Error();
+      if (!validate(input, dict65)) throw new Error('execution failed');
       return fromBin(decode(input, dict65));
     }
   }
 }
 
-const is65 = (s: string) => s.length === 65 && [...new Set(s)].length === 65;
+const is65 = (dict65: string) => {
+  const dict = s2(dict65);
+  return dict.length === 65 && [...new Set(dict)].length === 65;
+};
 
-const errorStr = '<错误>'.split('').join('\u200b');
+const errorStr = s2('<错误>').join('\u200b');
 
 function App() {
   const [dict65, setDict65] = useState(
-    '富强民主文明和谐自由平等公正法治爱国敬业诚信友善热祖为荣服务人崇尚科学辛勤劳动好团结互助实守见义遵纪艰苦奋斗骄赢奇迹感恩进步梦想坚'
+    '富强民主文明和谐自由平等公正法治爱国敬业诚信友善热祖为荣服务人崇尚科学辛勤劳动好团结互助实守见义遵纪艰苦奋斗骄赢奇迹感恩进步梦想😊'
   );
   const [input, setInput] = useState('');
   const [op, setOp] = useState(Op.Encode);
@@ -76,10 +79,10 @@ function App() {
     <div>
       <Grid
         container
-        component='div'
-        direction='column'
-        justify='center'
-        alignItems='center'
+        component="div"
+        direction="column"
+        justify="center"
+        alignItems="center"
         spacing={1}
         style={{ margin: '20px 0' }}
       >
@@ -87,11 +90,11 @@ function App() {
           <TextField
             multiline
             rows={5}
-            label='字典'
-            variant='outlined'
+            label="字典"
+            variant="outlined"
             value={dict65}
             error={!is65(dict65)}
-            helperText='65个唯一字符'
+            helperText="65个唯一字符"
             style={{ width: 300 }}
             onChange={(e) => setDict65(e.currentTarget.value)}
           />
@@ -100,8 +103,8 @@ function App() {
           <TextField
             multiline
             rows={10}
-            label='输入'
-            variant='outlined'
+            label="输入"
+            variant="outlined"
             value={input}
             error={output === errorStr && is65(dict65)}
             style={{ width: 300 }}
@@ -109,13 +112,15 @@ function App() {
           />
         </Grid>
         <Grid item>
-          <Grid container component='label' alignItems='center'>
+          <Grid container component="label" alignItems="center">
             <Grid item>编码</Grid>
             <Grid item>
               <Switch
-                color='primary'
+                color="primary"
                 checked={op === Op.Decode}
-                onChange={() => setOp(op === Op.Decode ? Op.Encode : Op.Decode)}
+                onChange={() =>
+                  setOp((op) => (op === Op.Decode ? Op.Encode : Op.Decode))
+                }
               />
             </Grid>
             <Grid item>解码</Grid>
@@ -125,16 +130,18 @@ function App() {
           <TextField
             multiline
             rows={10}
-            label='输出'
-            variant='outlined'
+            label="输出"
+            variant="outlined"
             value={output}
             style={{ width: 300 }}
             InputProps={{ readOnly: true }}
             inputRef={outputRef}
+            error={output === errorStr}
             onClick={() => {
-              if (output) {
+              if (output && output !== errorStr) {
                 outputRef.current?.select();
-                document.execCommand('copy');
+                if (navigator.clipboard) navigator.clipboard.writeText(output);
+                else document.execCommand('copy');
               }
             }}
           />
